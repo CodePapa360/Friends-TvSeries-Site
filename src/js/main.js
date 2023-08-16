c681afeb5f07f28ad57c4ddace81b23a733e4248
codepapa360 <codepapa360@gmail.com>
2023-08-17 00:43:47 +0600

    designed-serchBar&seasonView

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-import "./components/header";
+const navMenu = document.querySelector(".nav");
+const btnMenu = document.querySelector(".hamburger-menu");
+const overlay = document.querySelector(".overlay");
+
+// Navigation menu
+[btnMenu, overlay].forEach((el) =>
+  el.addEventListener("click", () => {
+    navMenu.classList.toggle("open");
+    overlay.classList.toggle("open");
+  }),
+);
